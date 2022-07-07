d66d48c0f23983f853bbc9300e12c7291366b871
9Mickle <zaks-00@mail.ru>
2022-07-07 17:43:41 +0300

    fixed Controller on RestController

--- src/main/java/com/example/arttek/controller/FirmController.java
+++ src/main/java/com/example/arttek/controller/FirmController.java
@@ -8,36 +8,25 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Objects;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/firm")
+@CrossOrigin(origins = "http://localhost:3000")
 @AllArgsConstructor
 public class FirmController {
     private final FirmService firmService;
-    private static Firm transitFirm;
-
-    @Operation(summary = "Домашняя страница с поиском фирмы по номеру ATI")
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
 
     @Operation(summary = "Страница с информацией о фирме")
     @GetMapping("/info")
-    public String getFullInfo(Model model, @RequestParam String id) {
+    public ResponseEntity<Object> getFullInfo(@RequestParam String atiId) {
         final String TOKEN = "88e0d871c901484b99377e3c39744c66";
-        String firmSummaryGET = "https://api.ati.su/v1.0/firms/summary/" + id;
-        String firmContactGET = String.format("https://api.ati.su/v1.0/firms/%s/contacts/summary", id);
+        String firmSummaryGET = "https://api.ati.su/v1.0/firms/summary/" + atiId;
+        String firmContactGET = String.format("https://api.ati.su/v1.0/firms/%s/contacts/summary", atiId);
 
         RestTemplate template = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -47,25 +36,16 @@ public class FirmController {
         Firm firm = template.exchange(firmSummaryGET, HttpMethod.GET, entity, Firm.class).getBody();
         Contact[] contacts = template.exchange(firmContactGET, HttpMethod.GET, entity, Contact[].class).getBody();
 
-        transitFirm = firm;
-        if (contacts != null && transitFirm != null) {
-            transitFirm.setContacts(List.of(contacts));
+        if (contacts != null && firm != null) {
+            firm.setContacts(List.of(contacts));
         }
 
-        model.addAttribute("firm", firm);
-        model.addAttribute("contacts", contacts);
-        model.addAttribute("documents", Objects.requireNonNull(firm).getFirm_documents());
-
-        return "firm_info";
+        return ResponseEntity.ok(firm);
     }
 
     @Operation(summary = "Сохранение всех данных о фирме")
     @PostMapping("/")
-    public String saveFirm() {
-        if (transitFirm != null) {
-            firmService.save(transitFirm);
-            return "successful_saving";
-        }
-        return "unsuccessful_saving";
+    public ResponseEntity<Object> saveFirm(@RequestBody Firm firm) {
+        return ResponseEntity.ok(firmService.save(firm));
     }
 }
